9617066366d012e74a53bcfa9f1fd601a2f427c0
Mohammad Atikur Rhaman <atikur.rhaman@enosisbd.com>
2024-03-16 12:02:53 +0600

    Learn about how to read & write file in node.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const fileOps = async() => {
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+        console.log(data);
+
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data); 
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet your'); 
+        await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt')); 
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
+        console.log(newData);
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+fileOps();
